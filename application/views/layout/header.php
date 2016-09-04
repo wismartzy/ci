b1e4f196f72ccffd924c7281d2ffa7f9e1c54ab6
wismartzy <wismartzy@gmail.com>
2016-09-04 21:07:15 +0800

    ADD: First

---
+++ application/views/layout/header.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wismartzy
+ * Date: 2016/9/4
+ * Time: 19:15
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<!-- Meta, title, CSS, favicons, etc. -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Gentellela Alela! | </title>
+
+	<!-- Bootstrap -->
+	<link href="<?php echo base_url('resource/gentelella'); ?>/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+	<!-- Font Awesome -->
+	<link href="<?php echo base_url('resource/gentelella'); ?>/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+	<!-- NProgress -->
+	<link href="<?php echo base_url('resource/gentelella'); ?>/vendors/nprogress/nprogress.css" rel="stylesheet">
+
+	<!-- Custom Theme Style -->
+	<link href="<?php echo base_url('resource/gentelella'); ?>/build/css/custom.min.css" rel="stylesheet">
+</head>
+<body class="nav-md">
+<div class="container body">
+    <div class="main_container">
+        <div class="col-md-3 left_col">
+            <div class="left_col scroll-view">
+                <div class="navbar nav_title" style="border: 0;">
+                    <a href="<?php echo base_url();?>" class="site_title"><i class="fa fa-paw"></i> <span>Gentellela Alela!</span></a>
+                </div>
+
+                <div class="clearfix"></div>
+
+                <!-- menu profile quick info -->
+                <div class="profile">
+                    <div class="profile_pic">
+                        <img src="<?php echo base_url('resource/gentelella/production'); ?>/images/img.jpg" alt="..." class="img-circle profile_img">
+                    </div>
+                    <div class="profile_info">
+                        <span>Welcome,</span>
+                        <h2>John Doe</h2>
+                    </div>
+                </div>
+                <!-- /menu profile quick info -->
+
+                <br />
